5ae396a8d5a737854f485609be9ef738ad90b843
Lucas Schmidt <lucas.schmidt015@gmail.com>
2023-03-10 22:36:04 -0300

    Configured environment variables

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
+require('dotenv').config();
 const express = require('express');
+const axios = require('axios');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
 const port = 3001;
+const clienteId = process.env.CLIENT_ID || 'Not-Found enviroment variable';
+const clienteSecret = process.env.CLIENT_SECRET || 'Not-Found enviroment variable';
+const redirectURL = process.env.REDIRECT_URL || 'Not-Found enviroment variable'; 
+
+const AssesToken = '';
 
 app.use(bodyParser.json());
 app.use(cors());
+
+
+function handleLogin(){
+    
+}
 
 app.get('/', (req, res) => {
     res.send('Hello World');
